bd5a68a998283d9f89f255edab25d52302be233a
goga <goga@localhost>
2017-11-19 17:36:31 +0300

    Запускаемая пустыщка ws.mcc.launcher.rcp

--- ru.toxsoft.mcc.ws.launcher.rcp/src/ru/toxsoft/mcc/ws/launcher/rcp/Activator.java
+++ ru.toxsoft.mcc.ws.launcher.rcp/src/ru/toxsoft/mcc/ws/launcher/rcp/Activator.java
@@ -1,11 +1,7 @@
 package ru.toxsoft.mcc.ws.launcher.rcp;
 
-import static ru.toxsoft.tslib.datavalue.impl.DvUtils.*;
-
 import ru.toxsoft.mws.base.mwsservice.ITsModularWorkstationService;
-import ru.toxsoft.mws.module.tsproject.IMwsModuleTsProjectConstants;
 import ru.toxsoft.tsgui.e4.app.TsActivator;
-import ru.toxsoft.tslib.patterns.txtprojs.proj.TsProjectFileFormatInfo;
 
 /**
  * Активатор плагина.
@@ -19,14 +15,16 @@ public class Activator
    * Идентификатор плагина.
    */
   public static final String PLUGIN_ID = "ru.toxsoft.mcc.ws.launcher.rcp"; //$NON-NLS-1$
-  private static Activator   instance  = null;
 
-  private static final String APP_SETTINGS_ROOT_NAME    = "/com/hazard157/psx4"; //$NON-NLS-1$
-  private static final int    PROJ_FILE_FORMAT_VERSTION = 4;
-  private static final String PROJ_FILE_APP_ID          = "com.hazard157.psx4";  //$NON-NLS-1$
+  /**
+   * Экземпляр этого плагина - синглтона.
+   */
+  private static Activator instance = null;
 
-  private static final TsProjectFileFormatInfo PFF_INFO =
-      new TsProjectFileFormatInfo( PROJ_FILE_APP_ID, PROJ_FILE_FORMAT_VERSTION );
+  /**
+   * Название ключа настроек программы в реестре ОС.
+   */
+  private static final String APP_SETTINGS_ROOT_NAME = "/ru/toxsoft/mcc"; //$NON-NLS-1$
 
   /**
    * Пустой конструктор.
@@ -43,8 +41,6 @@ public class Activator
   @Override
   protected void doStart() {
     ITsModularWorkstationService mws = Activator.getInstance().getOsgiService( ITsModularWorkstationService.class );
-    IMwsModuleTsProjectConstants.ALWAYS_USE_FILE_MENU.setValue( mws.mwsContext().params(), DV_TRUE );
-    IMwsModuleTsProjectConstants.REF_PROJECT_FILE_FORMAT_INFO.setValue( mws.mwsContext(), PFF_INFO );
     mws.setAppSettingsRootName( APP_SETTINGS_ROOT_NAME );
   }
 
---
+++ ru.toxsoft.mcc.ws.launcher.rcp/src/ru/toxsoft/mcc/ws/launcher/rcp/app/addons/IMccResources.java
@@ -0,0 +1,16 @@
+package ru.toxsoft.mcc.ws.launcher.rcp.app.addons;
+
+/**
+ * Локализуемые ресурсы.
+ *
+ * @author goga
+ */
+@SuppressWarnings( "nls" )
+interface IMccResources {
+
+  /**
+   * {@link AddonMccRcp}
+   */
+  String WINDOW_TITLE = "АРМ МосКокс";
+
+}
