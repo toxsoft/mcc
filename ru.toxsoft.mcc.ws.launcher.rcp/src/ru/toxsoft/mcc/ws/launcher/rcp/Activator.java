eb1c5ac90536c59b4fad6120e5694a3d7052e54a
mkoks <mkoks@MOSKOKS>
2018-07-10 13:49:22 +0300

    Указание в запускающем проекте необходимой модели дерева объектов

--- ru.toxsoft.mcc.ws.launcher.rcp/src/ru/toxsoft/mcc/ws/launcher/rcp/Activator.java
+++ ru.toxsoft.mcc.ws.launcher.rcp/src/ru/toxsoft/mcc/ws/launcher/rcp/Activator.java
@@ -9,7 +9,10 @@ import ru.toxsoft.mws.module.s5.conn.EOpenCmdStartegy;
 import ru.toxsoft.mws.module.s5.conn.IMwsModuleS5ConnConstants;
 import ru.toxsoft.mws.module.s5.conn.cfg.IS5ConnParameters;
 import ru.toxsoft.mws.module.s5.conn.cfg.impl.S5ConnConfig;
+import ru.toxsoft.mws.module.s5.journals.ICfgConstants;
+import ru.toxsoft.mws.module.s5.journals.models.OdsFileObjModel;
 import ru.toxsoft.tsgui.e4.app.TsActivator;
+import ru.toxsoft.tslib.datavalue.impl.DvUtils;
 import ru.toxsoft.tslib.utils.login.ILoginInfo;
 import ru.toxsoft.tslib.utils.login.LoginInfo;
 import ru.toxsoft.tslib.utils.version.DefaultTsVersion;
@@ -20,77 +23,83 @@ import ru.toxsoft.tslib.utils.version.ITsVersion;
  *
  * @author goga
  */
-public class Activator extends TsActivator {
+public class Activator
+    extends TsActivator {
 
-	/**
-	 * Идентификатор плагина.
-	 */
-	public static final String PLUGIN_ID = "ru.toxsoft.mcc.ws.launcher.rcp"; //$NON-NLS-1$
+  /**
+   * Идентификатор плагина.
+   */
+  public static final String PLUGIN_ID = "ru.toxsoft.mcc.ws.launcher.rcp"; //$NON-NLS-1$
 
-	/**
-	 * Экземпляр этого плагина - синглтона.
-	 */
-	private static Activator instance = null;
+  /**
+   * Экземпляр этого плагина - синглтона.
+   */
+  private static Activator instance = null;
 
-	/**
-	 * Название ключа настроек программы в реестре ОС.
-	 */
-	// private static final String APP_SETTINGS_ROOT_NAME = "/ru/toxsoft/mcc";
-	// //$NON-NLS-1$
+  /**
+   * Название ключа настроек программы в реестре ОС.
+   */
+  // private static final String APP_SETTINGS_ROOT_NAME = "/ru/toxsoft/mcc";
+  // //$NON-NLS-1$
 
-	// -----------------------------
-	String STR_N_APP = "МОСКОКС";
-	String STR_D_APP = "Модульный клиент системы МОСКОКС";
+  // -----------------------------
+  String STR_N_APP = "МОСКОКС";
+  String STR_D_APP = "Модульный клиент системы МОСКОКС";
 
-	String APP_ID = "ru.toxsoft.mcc.mws";
-	String APP_ALIAS = "mcc";
-	ITsVersion APP_VERSION = new DefaultTsVersion(1, 0);
+  String APP_ID = "ru.toxsoft.mcc.mws";
+  String APP_ALIAS = "mcc";
+  ITsVersion APP_VERSION = new DefaultTsVersion( 1, 0 );
 
-	ITsApplicationInfo APP_INFO = new TsApplicationInfo(APP_ID, STR_N_APP, STR_D_APP, APP_ALIAS, APP_VERSION);
-	// ------------------------------
+  ITsApplicationInfo APP_INFO = new TsApplicationInfo( APP_ID, STR_N_APP, STR_D_APP, APP_ALIAS, APP_VERSION );
+  // ------------------------------
 
-	/**
-	 * Пустой конструктор.
-	 */
-	public Activator() {
-		super(PLUGIN_ID);
-		checkInstance(instance);
-		instance = this;
-	}
+  /**
+   * Пустой конструктор.
+   */
+  public Activator() {
+    super( PLUGIN_ID );
+    checkInstance( instance );
+    instance = this;
+  }
 
-	/**
-	 * Настраивает конфигурацию модулей, которые будут запскаться.
-	 */
-	@SuppressWarnings("nls")
-	@Override
-	protected void doStart() {
-		ITsModularWorkstationService mws = Activator.getInstance().getOsgiService(ITsModularWorkstationService.class);
-		mws.setAppInfo(APP_INFO);
-		// mws.setAppSettingsRootName( APP_SETTINGS_ROOT_NAME );
-		// настройка умолчаний соединения с сервером (модуль mws.module.s5.conn)
-		mws.mwsContext().params().setBool(IMwsModuleS5ConnConstants.ALWAYS_USE_FILE_MENU, true);
+  /**
+   * Настраивает конфигурацию модулей, которые будут запскаться.
+   */
+  @SuppressWarnings( "nls" )
+  @Override
+  protected void doStart() {
+    ITsModularWorkstationService mws = Activator.getInstance().getOsgiService( ITsModularWorkstationService.class );
+    mws.setAppInfo( APP_INFO );
+    // mws.setAppSettingsRootName( APP_SETTINGS_ROOT_NAME );
+    // настройка умолчаний соединения с сервером (модуль mws.module.s5.conn)
+    mws.mwsContext().params().setBool( IMwsModuleS5ConnConstants.ALWAYS_USE_FILE_MENU, true );
 
-		S5ConnConfig scc = new S5ConnConfig();
-		scc.params().setStr(IS5ConnParameters.API_BEAN_NAME, IMccServerApi.API_BEAN_NAME);
-		scc.params().setStr(IS5ConnParameters.API_INTERFACE, IMccServerApi.API_INTERFACE_NAME);
-		scc.params().setStr(IS5ConnParameters.MODULE_NAME, IMccServerApi.APP_MODULE_NAME);
-		scc.params().setStr(IS5ConnParameters.CLIENT_SERVICE_INITER_CLASS_NAME, MccServicesInitializer.class.getName());
-		scc.params().setStr(IS5ConnParameters.HOST_NAME, "localhost");
+    S5ConnConfig scc = new S5ConnConfig();
+    scc.params().setStr( IS5ConnParameters.API_BEAN_NAME, IMccServerApi.API_BEAN_NAME );
+    scc.params().setStr( IS5ConnParameters.API_INTERFACE, IMccServerApi.API_INTERFACE_NAME );
+    scc.params().setStr( IS5ConnParameters.MODULE_NAME, IMccServerApi.APP_MODULE_NAME );
+    scc.params().setStr( IS5ConnParameters.CLIENT_SERVICE_INITER_CLASS_NAME, MccServicesInitializer.class.getName() );
+    scc.params().setStr( IS5ConnParameters.HOST_NAME, "localhost" );
 
-		IMwsModuleS5ConnConstants.CONFIG_DEFAULTS.setValue(mws.mwsContext().params(), scc);
+    IMwsModuleS5ConnConstants.CONFIG_DEFAULTS.setValue( mws.mwsContext().params(), scc );
 
-		ILoginInfo dli = new LoginInfo("root", "1");
-		IMwsModuleS5ConnConstants.DEFAULT_LOGIN_INFO.setValue(mws.mwsContext().params(), dli);
-		IMwsModuleS5ConnConstants.OPEN_CMD_STRATEGY.setValue(mws.mwsContext().params(), EOpenCmdStartegy.OPEN_LAST);
-	}
+    ILoginInfo dli = new LoginInfo( "root", "1" );
+    IMwsModuleS5ConnConstants.DEFAULT_LOGIN_INFO.setValue( mws.mwsContext().params(), dli );
+    IMwsModuleS5ConnConstants.OPEN_CMD_STRATEGY.setValue( mws.mwsContext().params(), EOpenCmdStartegy.OPEN_LAST );
 
-	/**
-	 * Возвращает ссылку на единственный экземпляр этого активатора.
-	 *
-	 * @return {@link Activator} - ссылка на единственный экземпляр
-	 */
-	public static Activator getInstance() {
-		return instance;
-	}
+    // установка модели дерева объектов
+    ICfgConstants.EVENTS_FILTER_OBJECTS_TREE_MODEL.setValue( mws.mwsContext().params(),
+        DvUtils.avStr( OdsFileObjModel.class.getName() ) );
+
+  }
+
+  /**
+   * Возвращает ссылку на единственный экземпляр этого активатора.
+   *
+   * @return {@link Activator} - ссылка на единственный экземпляр
+   */
+  public static Activator getInstance() {
+    return instance;
+  }
 
 }
