79f158c402d71e64c2f6bb274cae9f3ee5753d3f
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2022-10-25 15:36:58 +0300

    очептяки исправлены

--- org.toxsoft.mcc.server/dist/run_mcc_server.sh
+++ org.toxsoft.mcc.server/dist/run_mcc_server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # export JAVA_HOME=/usr/java/jdk1.8.0_211-amd64
-export JAVA_HOME=/usr/java/jdk-11.0.7
-export WILDFLY_HOME=/home/tsdev/mcc/tswildfly-18.0.1.Final/bin/
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export WILDFLY_HOME=/home/dmitry/works/tswildfly-26.1.1.Final/bin/
 export LC_ALL=ru_RU.UTF-8
 
 # Префикс проекта
-export PROJECT=mmc
+export PROJECT=mcc
 
 # Имя узла кластера
 # export NODE=local
@@ -20,7 +20,7 @@ rm -Rfv ../standalone-${PROJECT}-${NODE}/tmp
 
 # Запуск сервера
 # ./standalone.sh -c standalone-${PROJECT}-${NODE}.xml -b 0.0.0.0 -Djboss.server.base.dir=../standalone-${PROJECT}-${NODE} -Djboss.server.name=${PROJECT}_${NODE} &
-./standalone.sh -c standalone-${PROJECT}-${NODE}.xml -b 0.0.0.0 -Djboss.server.base.dir=../standalone-${PROJECT}-${NODE} -Djboss.server.name=${PROJECT}_${NODE} --debug *:8787  &
+./standalone.sh -c standalone-${PROJECT}-${NODE}.xml -b localhost -Djboss.server.base.dir=../standalone-${PROJECT}-${NODE} -Djboss.server.name=${PROJECT}_${NODE} --debug *:8787  &
 # ./standalone.sh -c standalone-${PROJECT}.xml -b 0.0.0.0 -Djboss.server.base.dir=../standalone-${PROJECT}-${NODE} -Djboss.server.name=${PROJECT}_${NODE} --debug *:8787 &
 
 
