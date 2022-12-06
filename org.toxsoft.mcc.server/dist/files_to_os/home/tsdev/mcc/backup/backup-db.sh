ffe7fa0bb53db4d399ea15bca34901f50ef0fcd0
tsdev <tsdev@VJ-MCC-2>
2022-12-06 03:48:41 +0300

    org.toxsoft.mcc => ru.toxsoft.mcc. Part 1

--- org.toxsoft.mcc.server/dist/files_to_os/home/tsdev/mcc/backup/backup-db.sh
+++
@@ -1,9 +0,0 @@
-# mysqldump --user=root --password=1 tm > tm.sql.
-
-# mysqldump --user=root --password=1 tm2 | pv | gzip >  tm2-$(date +%Y%m%d).sql.gz
-
-
-mysqldump --user=root --password=1 sitrol_mcc > sitrol_mcc-$(date +%Y-%m-%d).sql
-
-
-
--- org.toxsoft.mcc.server/dist/files_to_os/home/tsdev/s5admin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pushd  /home/tsdev/mm/s5admin/s5admin
-
-./s5admin-mm-fgdp.sh
-
-popd
