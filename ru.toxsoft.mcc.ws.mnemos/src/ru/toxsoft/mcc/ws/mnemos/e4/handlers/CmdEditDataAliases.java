96de3306c1072490700ff5a0564dd6851218d200
goga <goga@GOGAW-PC-LIIN>
2022-12-21 08:48:02 +0300

    Добавлен пункт меню правки Data Aliases

---
+++ ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/e4/handlers/CmdEditDataAliases.java
@@ -0,0 +1,23 @@
+package ru.toxsoft.mcc.ws.mnemos.e4.handlers;
+
+import org.eclipse.e4.core.di.annotations.*;
+import org.eclipse.swt.widgets.*;
+import org.toxsoft.core.tsgui.dialogs.*;
+
+/**
+ * Испонитель команды "Правки названии параметров".
+ *
+ * @author hazard157
+ */
+public class CmdEditDataAliases {
+
+  @Execute
+  void exec( Shell aShell ) {
+
+    // TODO CmdEditDataAliases.exec()
+
+    TsDialogUtils.underDevelopment( aShell );
+
+  }
+
+}
