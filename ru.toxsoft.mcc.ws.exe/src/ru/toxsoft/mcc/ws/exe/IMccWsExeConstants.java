e8926c8acbe5713bd0358bd1a9a4083827ef65f8
goga <goga@GOGAH-PC-LIN>
2022-12-22 15:59:52 +0300

    Обновление информации о версии в IMccWsExeConstants

--- ru.toxsoft.mcc.ws.exe/src/ru/toxsoft/mcc/ws/exe/IMccWsExeConstants.java
+++ ru.toxsoft.mcc.ws.exe/src/ru/toxsoft/mcc/ws/exe/IMccWsExeConstants.java
@@ -21,7 +21,7 @@ public interface IMccWsExeConstants {
   String APP_ID    = "ru.toxsoft.mcc"; //$NON-NLS-1$
   String APP_ALIAS = "mcc";            //$NON-NLS-1$
 
-  TsVersion APP_VERSION = new TsVersion( 0, 9, 2022, Month.OCTOBER, 27 );
+  TsVersion APP_VERSION = new TsVersion( 1, 0, 2022, Month.DECEMBER, 22 );
 
   ITsApplicationInfo APP_INFO = new TsApplicationInfo( APP_ID, STR_N_APP_INFO, STR_D_APP_INFO, APP_ALIAS, APP_VERSION );
 
