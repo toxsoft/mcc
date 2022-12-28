5a99e69a492fcc8b5e4ad7591dc5d16a419f8c9a
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2022-12-28 10:27:12 +0300

    начало использования OneWsService

--- ru.toxsoft.mcc.ws.exe/src/ru/toxsoft/mcc/ws/exe/e4/addons/AddonMccWsExe.java
+++ ru.toxsoft.mcc.ws.exe/src/ru/toxsoft/mcc/ws/exe/e4/addons/AddonMccWsExe.java
@@ -7,9 +7,13 @@ import org.eclipse.e4.core.contexts.*;
 import org.eclipse.e4.ui.model.application.*;
 import org.eclipse.e4.ui.model.application.ui.basic.*;
 import org.eclipse.e4.ui.workbench.modeling.*;
+import org.toxsoft.core.tsgui.bricks.quant.*;
 import org.toxsoft.core.tsgui.graphics.icons.impl.*;
 import org.toxsoft.core.tsgui.mws.*;
+import org.toxsoft.core.tsgui.mws.Activator;
 import org.toxsoft.core.tsgui.mws.bases.*;
+import org.toxsoft.uskat.onews.gui.*;
+import org.toxsoft.uskat.users.gui.*;
 
 /**
  * Application addon.
@@ -24,6 +28,13 @@ public class AddonMccWsExe
    */
   public AddonMccWsExe() {
     super( Activator.PLUGIN_ID );
+  }
+
+  @Override
+  protected void doRegisterQuants( IQuantRegistrator aQuantRegistrator ) {
+    // регистрация М5 моделей работы с пользователями и правами доступа
+    aQuantRegistrator.registerQuant( new QuantSkUsersGui() );
+    aQuantRegistrator.registerQuant( new QuantSkOneWsGui() );
   }
 
   @Override
