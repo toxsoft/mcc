71ff8556541b7c5787985c306b671b54dcda11a0
vs <vs@LAPTOP-DSEKI2TA>
2023-01-13 16:42:10 +0300

    Bug fix

--- ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/app/rtb/TestPanel.java
+++ ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/app/rtb/TestPanel.java
@@ -3,7 +3,6 @@ package ru.toxsoft.mcc.ws.mnemos.app.rtb;
 import org.eclipse.swt.*;
 import org.eclipse.swt.custom.*;
 import org.eclipse.swt.widgets.*;
-import org.toxsoft.core.tsgui.graphics.*;
 import org.toxsoft.core.tsgui.panels.vecboard.*;
 import org.toxsoft.core.tsgui.panels.vecboard.impl.*;
 import org.toxsoft.core.tsgui.widgets.*;
@@ -35,19 +34,20 @@ public class TestPanel {
 
   public Composite createControl( Composite aParent ) {
     // VecColumnLayout l = new VecColumnLayout( 3, false );
-    VecColumnLayout l = VecColumnLayout.createNoTrims( 3, false );
-
-    l.addControl( new CLabelWrapper( "cell 1:1", SWT.BORDER ),
-        new VecColumnLayoutData( EHorAlignment.RIGHT, EVerAlignment.BOTTOM ) );
-    l.addControl( new CLabelWrapper( "cell 1:2", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-    l.addControl( new CLabelWrapper( "cell 1:3", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-    l.addControl( new CLabelWrapper( "cell 2:1", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-    l.addControl( new CLabelWrapper( "cell 2:2", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-    l.addControl( new CLabelWrapper( "cell 2:3", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-    l.addControl( new CLabelWrapper( "cell 3:1", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
-
-    vecBoard.setLayout( l );
-    return vecBoard.createControl( aParent );
+    // VecColumnLayout l = VecColumnLayout.createNoTrims( 3, false );
+    //
+    // l.addControl( new CLabelWrapper( "cell 1:1", SWT.BORDER ),
+    // new VecColumnLayoutData( EHorAlignment.RIGHT, EVerAlignment.BOTTOM ) );
+    // l.addControl( new CLabelWrapper( "cell 1:2", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    // l.addControl( new CLabelWrapper( "cell 1:3", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    // l.addControl( new CLabelWrapper( "cell 2:1", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    // l.addControl( new CLabelWrapper( "cell 2:2", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    // l.addControl( new CLabelWrapper( "cell 2:3", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    // l.addControl( new CLabelWrapper( "cell 3:1", SWT.BORDER ), IVecColumnLayoutData.DEFAULT );
+    //
+    // vecBoard.setLayout( l );
+    // return vecBoard.createControl( aParent );
+    return new Composite( aParent, SWT.BORDER );
   }
 
 }
