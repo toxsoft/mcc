3ecd2c01a5f616d1608169bac63a29f5e24c7ad3
tsdev <tsdev@VJ-MCC-2>
2023-01-17 13:18:12 +0300

    подстройка размера панели алармов

--- ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/e4/uiparts/UipartMccMainScheme.java
+++ ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/e4/uiparts/UipartMccMainScheme.java
@@ -1,16 +1,16 @@
 package ru.toxsoft.mcc.ws.mnemos.e4.uiparts;
 
-import java.awt.*;
+import java.awt.Dimension;
 
-import org.eclipse.swt.*;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.toxsoft.core.tsgui.utils.layout.*;
+import org.toxsoft.core.tsgui.utils.layout.AWTLayout;
 import org.toxsoft.core.tsgui.utils.layout.BorderLayout;
-import org.toxsoft.core.tsgui.widgets.*;
-import org.toxsoft.core.tslib.utils.logs.impl.*;
-import org.toxsoft.uskat.base.gui.conn.*;
-import org.toxsoft.uskat.base.gui.e4.uiparts.*;
-import org.toxsoft.uskat.core.connection.*;
+import org.toxsoft.core.tsgui.widgets.TsComposite;
+import org.toxsoft.core.tslib.utils.logs.impl.LoggerUtils;
+import org.toxsoft.uskat.base.gui.conn.ISkConnectionSupplier;
+import org.toxsoft.uskat.base.gui.e4.uiparts.SkMwsAbstractPart;
+import org.toxsoft.uskat.core.connection.ISkConnection;
 
 import ru.toxsoft.mcc.ws.mnemos.app.*;
 
@@ -108,7 +108,7 @@ public class UipartMccMainScheme
 
     MccAlarmsPanelHolder alarmsHolder = new MccAlarmsPanelHolder( leftComp, skConn(), tsContext() );
     alarmsHolder.setLayoutData( BorderLayout.SOUTH );
-    alarmsHolder.setData( AWTLayout.KEY_PREFERRED_SIZE, new Dimension( -1, 105 ) );
+    alarmsHolder.setData( AWTLayout.KEY_PREFERRED_SIZE, new Dimension( -1, 120 ) );
 
     MccSchemeRightPanel rightComp = new MccSchemeRightPanel( aParent, tsContext() );
     rightComp.setLayoutData( BorderLayout.EAST );
