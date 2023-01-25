092db424acc011cf6da7bc9f0caff260b5cb452c
vs <vs@LAPTOP-DSEKI2TA>
2023-01-25 21:24:10 +0300

    Доработка мнемосхемы

--- ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/e4/uiparts/UipartMccMainScheme.java
+++ ru.toxsoft.mcc.ws.mnemos/src/ru/toxsoft/mcc/ws/mnemos/e4/uiparts/UipartMccMainScheme.java
@@ -1,16 +1,16 @@
 package ru.toxsoft.mcc.ws.mnemos.e4.uiparts;
 
-import java.awt.Dimension;
+import java.awt.*;
 
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.*;
 import org.eclipse.swt.widgets.Composite;
-import org.toxsoft.core.tsgui.utils.layout.AWTLayout;
+import org.toxsoft.core.tsgui.utils.layout.*;
 import org.toxsoft.core.tsgui.utils.layout.BorderLayout;
-import org.toxsoft.core.tsgui.widgets.TsComposite;
-import org.toxsoft.core.tslib.utils.logs.impl.LoggerUtils;
-import org.toxsoft.uskat.base.gui.conn.ISkConnectionSupplier;
-import org.toxsoft.uskat.base.gui.e4.uiparts.SkMwsAbstractPart;
-import org.toxsoft.uskat.core.connection.ISkConnection;
+import org.toxsoft.core.tsgui.widgets.*;
+import org.toxsoft.core.tslib.utils.logs.impl.*;
+import org.toxsoft.uskat.base.gui.conn.*;
+import org.toxsoft.uskat.base.gui.e4.uiparts.*;
+import org.toxsoft.uskat.core.connection.*;
 
 import ru.toxsoft.mcc.ws.mnemos.app.*;
 
@@ -62,24 +62,24 @@ public class UipartMccMainScheme
     // schemeComp.addPaintListener( aEvent -> aEvent.gc.drawImage( imgScheme, 0, 0 ) );
 
     MccSchemePanel schemeComp = new MccSchemePanel( leftComp, tsContext() );
-    schemeComp.addAI( 145, 122, "n2AI_TP9" ); //$NON-NLS-1$
+    schemeComp.addAI( 145 - 2, 122, "n2AI_TP9" ); //$NON-NLS-1$
     schemeComp.addAI( 373, 119, "n2AI_TP8" ); //$NON-NLS-1$
-    schemeComp.addAI( 464, 120, "n2AI_TP7" ); //$NON-NLS-1$
+    schemeComp.addAI( 464 - 1, 120, "n2AI_TP7" ); //$NON-NLS-1$
     schemeComp.addAI( 644, 121, "n2AI_TP6" ); //$NON-NLS-1$
-    schemeComp.addAI( 466, 236, "n2AI_TP5" ); //$NON-NLS-1$
+    schemeComp.addAI( 466 - 1, 236, "n2AI_TP5" ); //$NON-NLS-1$
     schemeComp.addAI( 645, 235, "n2AI_TP4" ); //$NON-NLS-1$
     schemeComp.addAI( 737, 145, "n2AI_TP3" ); //$NON-NLS-1$
     schemeComp.addAI( 737, 229, "n2AI_TP2" ); //$NON-NLS-1$
     schemeComp.addAI( 917, 274, "n2AI_TP1" ); //$NON-NLS-1$
 
-    schemeComp.addAI( 146, 210, "n2AI_ST8" ); //$NON-NLS-1$
+    schemeComp.addAI( 146 - 3, 210, "n2AI_ST8" ); //$NON-NLS-1$
     schemeComp.addAI( 374, 212, "n2AI_ST7" ); //$NON-NLS-1$
     schemeComp.addAI( 521, 121, "n2AI_ST6" ); //$NON-NLS-1$
     schemeComp.addAI( 587, 120, "n2AI_ST5" ); //$NON-NLS-1$
     schemeComp.addAI( 522, 235, "n2AI_ST4" ); //$NON-NLS-1$
     schemeComp.addAI( 590, 236, "n2AI_ST3" ); //$NON-NLS-1$
     schemeComp.addAI( 793, 120, "n2AI_ST2" ); //$NON-NLS-1$
-    schemeComp.addAI( 915, 121, "n2AI_ST1" ); //$NON-NLS-1$
+    schemeComp.addAI( 915 + 1, 121, "n2AI_ST1" ); //$NON-NLS-1$
 
     schemeComp.addAI( 479, 638, "n2AI_TM2" ); //$NON-NLS-1$
     schemeComp.addAI( 484, 772, "n2AI_TM1" ); //$NON-NLS-1$
@@ -101,7 +101,7 @@ public class UipartMccMainScheme
     schemeComp.addAI( 1414, 237, "n2AI_T13" ); //$NON-NLS-1$
     schemeComp.addAI( 424, 293, "n2AI_T9" ); //$NON-NLS-1$
     schemeComp.addAI( 369, 294, "n2AI_T8" ); //$NON-NLS-1$
-    schemeComp.addAI( 146, 274, "n2AI_T7" ); //$NON-NLS-1$
+    schemeComp.addAI( 146 - 1, 274, "n2AI_T7" ); //$NON-NLS-1$
     schemeComp.addAI( 115, 378, "n2AI_T3" ); //$NON-NLS-1$
 
     schemeComp.addAI( 258, 268, "n2AI_IS" ); //$NON-NLS-1$
