3f7cf95d7ce9e36668f689938e915cb637549b6a
mvk <kovach.mike@gmail.com>
2023-04-05 16:57:29 +0700

    uskat: создание и обновление s5-классов/объектов на старте сервера

--- ru.toxsoft.mcc.server/dist/start-mcc-node1.cmd
+++ ru.toxsoft.mcc.server/dist/start-mcc-node1.cmd
@@ -1,12 +1,14 @@
 set JAVA_HOME=C:\Program Files\Java\jdk-17.0.4.1
 set WILDFLY_HOME=d:\works\tswildfly-26.1.1.Final 
 
-:: Префикс проекта
+:: project prefix
 set PROJECT=mcc
-:: Имя узла кластера
+:: cluster node name
 set NODE=node01
-:: Смещение порта
+:: port offest
 set PORT_OFFSET=0
+:: private interface (cluster & management)
+set PRIVATE_INTEFACE=192.168.1.105
 
 
 cd %WILDFLY_HOME%\bin
@@ -15,5 +17,14 @@ rmdir /S /Q ..\standalone-%PROJECT%-%NODE%\data
 rmdir /S /Q ..\standalone-%PROJECT%-%NODE%\tmp
 
 chcp 1251
-rem standalone.bat -c standalone-%PROJECT%-%NODE%.xml -b 0.0.0.0 -Djboss.server.base.dir=../standalone-%PROJECT%-%NODE% -Djboss.server.name=%PROJECT%_%NODE% -Djboss.socket.binding.port-offset=%PORT_OFFSET%
-standalone.bat -c standalone-%PROJECT%-%NODE%.xml -b localhost -Djboss.server.base.dir=../standalone-%PROJECT%-%NODE% -Djboss.server.name=%PROJECT%_%NODE% -Djboss.socket.binding.port-offset=%PORT_OFFSET% -Dfile.encoding=CP866
+echo -------------------------------------------------------------------------------
+echo %date% %time% starting standalone RETCODE: %ERRORLEVEL% >> toxsoft-%PROJECT%.log
+echo project = %PROJECT%, NODE = %NODE%, private interface = %PRIVATE_INTEFACE%
+echo -------------------------------------------------------------------------------
+timeout 3
+
+standalone.bat -c standalone-%PROJECT%-%NODE%.xml -b=0.0.0.0 -Djboss.server.base.dir=../standalone-%PROJECT%-%NODE% -Djboss.server.name=%PROJECT%.server.%NODE% -Djboss.bind.address.private=%PRIVATE_INTEFACE% -Djboss.socket.binding.port-offset=%PORT_OFFSET% -Dfile.encoding=CP866
+
+echo standalone RETCODE: %ERRORLEVEL%
+echo %date% %time% exit standalone RETCODE: %ERRORLEVEL% >> toxsoft-%PROJECT%.log
+pause
